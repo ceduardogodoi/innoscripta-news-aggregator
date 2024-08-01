a040b61fba47b5491693495c341454521b347968
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-08-01 15:46:13 -0300

    feat: environment variables verification

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,59 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
+  // News API variables
   NEWS_API_KEY: z
+    .string({
+      required_error: "Variable is required.",
+    })
+    .min(1, "Variable must not be empty."),
+
+  // The Guardian variables
+  THE_GUARDIAN_API_KEY: z
+    .string({
+      required_error: "Variable is required.",
+    })
+    .min(1, "Variable must not be empty."),
+
+  // The New York Times variables
+  NYT_API_KEY: z
+    .string({
+      required_error: "Variable is required.",
+    })
+    .min(1, "Variable must not be empty."),
+  NYT_API_SECRET: z
+    .string({
+      required_error: "Variable is required.",
+    })
+    .min(1, "Variable must not be empty."),
+  NYT_APP_ID: z
     .string({
       required_error: "Variable is required.",
     })
     .min(1, "Variable must not be empty."),
 });
 
-function validate() {
+function validateEnv() {
   const result = envSchema.safeParse(process.env);
   if (!result.success) {
     console.error("❌ Invalid environment variables:");
     const fieldErrors = result.error.flatten().fieldErrors;
     console.error(JSON.stringify(fieldErrors, null, 2));
+    console.error("Ending process...");
 
     process.exit(1);
   }
 }
 
-validate();
+validateEnv();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 export default nextConfig;
