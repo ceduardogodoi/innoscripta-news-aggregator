fa8d18c5f72f384259581868257d1ffc035e2389
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-08-01 22:29:01 -0300

    feat: initial color palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import type { PropsWithChildren } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { PropsWithChildren } from "react";
+
+import "@/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "dark-blue": "#0E1E32",
+        gray: "#2A2A2A",
+        "light-red": "#F6DDDC",
+        red: "#C31815",
+        page: "#E5E5E5",
       },
     },
   },
