0587060cd930cc898658102365de60f4c54a2289
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-08-02 21:16:31 -0300

    fix: border radius hover

--- src/components/organisms/article/article-header.tsx
+++ src/components/organisms/article/article-header.tsx
@@ -1,5 +1,9 @@
 import type { PropsWithChildren } from "react";
 
 export function ArticleHeader({ children }: PropsWithChildren) {
-  return <header className="relative h-40 overflow-hidden">{children}</header>;
+  return (
+    <header className="relative h-40 overflow-hidden rounded-t">
+      {children}
+    </header>
+  );
 }
