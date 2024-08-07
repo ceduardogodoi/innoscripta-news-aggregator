7e9432445997e24eff920c4bb7ee937b163a0cfa
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-08-06 21:52:22 -0300

    feat: remove favorites

--- src/components/organisms/favorite-sources/index.tsx
+++ src/components/organisms/favorite-sources/index.tsx
@@ -12,16 +12,46 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Skeleton } from "@/components/ui/skeleton";
 import { SOURCES_VALUE_LABEL_MAP, SourceValue } from "@/constants";
 import { storage } from "@/utils/storage";
 
+type FavoriteSourcesState = {
+  sources: SourceValue[];
+  isLoading: boolean;
+};
+
 export function FavoriteSources() {
-  const [favoriteSources, setFavoriteSources] = useState<SourceValue[]>([]);
+  const [favoriteSources, setFavoriteSources] = useState<FavoriteSourcesState>({
+    sources: [],
+    isLoading: true,
+  });
 
   useEffect(() => {
     const sources = storage.getFavoriteSources();
-    setFavoriteSources(sources);
+    setFavoriteSources((state) => ({
+      ...state,
+      sources,
+      isLoading: false,
+    }));
   }, []);
+
+  function handleRemoveFromFavorites(source: SourceValue) {
+    setFavoriteSources((state) => ({ ...state, isLoading: true }));
+
+    storage.removeFavoriteSource(source);
+
+    setFavoriteSources((state) => {
+      const updatedSources = state.sources.filter(
+        (stateSource) => stateSource !== source,
+      );
+      return {
+        ...state,
+        sources: updatedSources,
+        isLoading: false,
+      };
+    });
+  }
 
   return (
     <section>
@@ -34,23 +64,34 @@ export function FavoriteSources() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            {favoriteSources.map((favoriteSource) => (
-              <>
-                <TableCell key={favoriteSource}>
+          {favoriteSources.isLoading && (
+            <TableRow>
+              <TableCell className="w-[70%]">
+                <Skeleton className="h-10 w-full bg-gray-200" />
+              </TableCell>
+              <TableCell className="w-[30%]">
+                <Skeleton className="h-10 w-full bg-gray-200" />
+              </TableCell>
+            </TableRow>
+          )}
+
+          {!favoriteSources.isLoading &&
+            favoriteSources.sources.map((favoriteSource) => (
+              <TableRow>
+                <TableCell className="w-[70%]" key={favoriteSource}>
                   {SOURCES_VALUE_LABEL_MAP[favoriteSource]}
                 </TableCell>
-                <TableCell>
+                <TableCell className="w-[30%]">
                   <Button
                     title={`Remove "${SOURCES_VALUE_LABEL_MAP[favoriteSource]}" from favorites`}
                     variant="ghost"
+                    onClick={() => handleRemoveFromFavorites(favoriteSource)}
                   >
                     <TrashIcon className="text-red-500" />
                   </Button>
                 </TableCell>
-              </>
+              </TableRow>
             ))}
-          </TableRow>
         </TableBody>
       </Table>
     </section>
